3cfda3b00333aebb1e21ad1c9724ff00e1a53885
Mike Rogers <mrogers23458@gmail.com>
2021-12-20 12:55:16 -0800

    added styling to login page, started on login / register state and submit hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import { LoginPage } from './pages';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-        <LoginPage />
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+        </Routes>
     </div>
   );
 }
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,13 +1,59 @@
-import React from "react";
-import { Navbar } from "../components";
+//allowing react to use state while updating email and password
+import React, {useState} from "react";
+//React Router Navigation import
+import { useNavigate } from 'react-router-dom'
+//helper import
+import { validateEmail } from '../utils/helper'
+
+//styling import
+import '../css/loginStyle.css';
 
 export default function LoginPage(){
+
+    let navigate = useNavigate();
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [errorMessage, setErrorMessage] = useState('')
+
+    const handleFormSubmit = (e) => {
+        //prevent page from refreshing
+        e.preventDefault();
+
+        // check to make sure the user has entered a valid email structure with the helper of ValidEmail Function in helper.js
+        if (!validateEmail(email)) {
+            setErrorMessage('Email is required');
+            
+            //database logic maybe?
+            return;
+        }
+
+        //if user successfully registers or logs in re-direct logic goes here
+        navigate('/areaselect')
+    }
+
+
     return (
-        <div className="landing-main">
-            <Navbar />
+        <div className="LoginContainer">
             <header>
-                <h1>Some Test Text</h1>
+                <h1>Welcome to CleanApp!</h1>
             </header>
+            <div className="loginFormHolder">
+                <form className="loginForm">
+                    <label className="email-label" for="email">E-mail</label> 
+                    <input className="emailInput" type="email" placeholder="youremail@provider.com" name="email"></input>
+                    <label className="password-label" for="password">Password</label>
+                    <input className="passwordInput" type="password" placeholder="password" name="password"></input>
+                    <div className="loginButtons">
+                        <button className="regButton" type="submit" onSubmit={handleFormSubmit}>Register</button>
+                        <button className="logButton" type="submit" onSubmit={handleFormSubmit}>Login</button>
+                    </div>
+                </form>
+            </div>
+            <div className="extraLinks">
+                <a href="/forgotpassword">Forgot Password?</a>
+                <a href="/privacy">Privacy</a>
+            </div>
         </div>
     )
 }
