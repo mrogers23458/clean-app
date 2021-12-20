ea516fe30d0200d814ac74339997ccef011e4171
Mike Rogers <mrogers23458@gmail.com>
2021-12-20 15:46:28 -0800

    updated some styling installed apollo client dependency, and react router

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -12,9 +12,35 @@ export default function LoginPage(){
 
     let navigate = useNavigate();
 
+    //creating variables for state and setting their value to an empty string
+    //email and setEmail state
     const [email, setEmail] = useState('')
+    //password and setPassword state
     const [password, setPassword] = useState('')
+    //error message if form errors
     const [errorMessage, setErrorMessage] = useState('')
+    //success message if form is properly submitted
+    const [successMessage, setSuccessMessage] = useState('')
+
+    //function for changing state to update with user input
+    const handleInputChange = (e) => {
+
+        const { target } = e;
+        const inputType = target.name
+        const inputValue = target.inputValue
+
+        //update and set state based on user input in a field
+        if (inputType === 'email') {
+            setEmail(inputValue)
+        }
+
+        if (inputType ==='password') {
+            setPassword(inputValue)
+        }
+
+
+    }
+
 
     const handleFormSubmit = (e) => {
         //prevent page from refreshing
@@ -29,21 +55,21 @@ export default function LoginPage(){
         }
 
         //if user successfully registers or logs in re-direct logic goes here
-        navigate('/areaselect')
+        navigate('/area')
     }
 
 
     return (
-        <div className="LoginContainer">
+        <div className="loginContainer">
             <header>
                 <h1>Welcome to CleanApp!</h1>
             </header>
             <div className="loginFormHolder">
                 <form className="loginForm">
-                    <label className="email-label" for="email">E-mail</label> 
-                    <input className="emailInput" type="email" placeholder="youremail@provider.com" name="email"></input>
-                    <label className="password-label" for="password">Password</label>
-                    <input className="passwordInput" type="password" placeholder="password" name="password"></input>
+                    <label className="email-label" htmlFor="email">E-mail</label> 
+                    <input className="emailInput" type="email" placeholder="youremail@provider.com" value={email} name="email" onChange={handleInputChange}></input>
+                    <label className="password-label" htmlFor="password">Password</label>
+                    <input className="passwordInput" type="password" placeholder="password" value={password} name="password" onChange={handleInputChange}></input>
                     <div className="loginButtons">
                         <button className="regButton" type="submit" onSubmit={handleFormSubmit}>Register</button>
                         <button className="logButton" type="submit" onSubmit={handleFormSubmit}>Login</button>
