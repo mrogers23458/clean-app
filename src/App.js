8b412a43b5152fa0c91a066b3f7ed11c98312d84
Mike Rogers <mrogers23458@gmail.com>
2021-12-20 15:56:37 -0800

    installed apollo client dependency, and setup apollo client in app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
 import React from 'react';
-import { LoginPage } from './pages';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+  ApolloProvider
+} from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
+import { LoginPage } from './pages';
+
+const httpLink = createHttpLink({
+  uri: '/graphql'
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="App">
+    <ApolloProvider client={client}>
+      <div className="App">
         <Routes>
           <Route path="/" element={<LoginPage />} />
         </Routes>
     </div>
+    </ApolloProvider>
   );
 }
 
